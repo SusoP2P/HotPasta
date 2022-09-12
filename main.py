b6c5f434704c81fb75c5298a8b4f791ade15645a
SusoP2P <113068085+SusoP2P@users.noreply.github.com>
2022-09-12 05:00:41 +0200

    First valid code: - Basic function placeholders - WIP basic settings - First boot check

--- main.py
+++ main.py
@@ -4,8 +4,15 @@
 #
 #
 # Reserved for comments and fancy space
-import settings
-settings
+import defaultsettings
+import internalsettings
+import modules
+
+defaultsettings.check_first_boot()
 
 
+
+
+
+print("\n""-----------\n""Code finished succesfully")
 print("Hot Pasta - The Copy/Paste tool")
---
+++ modules.py
@@ -0,0 +1,37 @@
+#
+#
+#
+#
+# Reserved for comments and fancy space
+
+import configparser
+import defaultsettings
+import internalsettings
+
+
+def tutorial():
+    print("Tutorial prompted")
+
+
+def onboarding():
+    print("Onboarding promtped")
+
+
+def menu_show():
+    print("Menu shown")
+
+
+def overlay_show():
+    print("Overlay shown")
+
+
+def SetInternalSettings():
+    print("Internal Settings set")
+
+
+def DefaultSettings():
+    defaultsettings.CreateDefaultSettings()
+
+
+def GenericError():
+    print("Generic Error")
